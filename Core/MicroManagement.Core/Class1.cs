9bbaa4b8e8f3f2d83c97e622f2cc39b8b34c5081
Karem Sfar Gandoura <Karem.sfargandoura@expensya.com>
2023-05-13 23:09:12 +0100

    Add Project and TimeSession models

--- Core/MicroManagement.Core/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MicroManagement.Core
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Core/MicroManagement.Core/TimeSession.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroManagement.Core
+{
+    public record TimeSession
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndDate { get; set; }
+        public IList<Guid> ProjectIds { get; set; }
+    }
+}
