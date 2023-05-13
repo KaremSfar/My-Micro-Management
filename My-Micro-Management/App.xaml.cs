7403d63767f69ce040d9370f572d286a8229c133
Karem Sfar Gandoura <Karem.sfargandoura@expensya.com>
2023-05-13 19:27:41 +0100

    First commit

---
+++ My-Micro-Management/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace My_Micro_Management;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ My-Micro-Management/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace My_Micro_Management;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
