a8254d5a8472785901e2bd936ef53da6fbba0a48
Karem Sfar Gandoura <Karem.sfargandoura@expensya.com>
2023-05-14 10:22:29 +0100

    Add sizing of main window and basic layout

--- Application/My-Micro-Management/App.xaml.cs
+++ Application/My-Micro-Management/App.xaml.cs
@@ -2,10 +2,23 @@
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new AppShell();
+    }
 
-		MainPage = new AppShell();
-	}
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+#if WINDOWS
+        const int newWidth = 1000;
+        const int newHeight = 600;
+
+        window.Width = newWidth;
+        window.Height = newHeight;
+#endif
+        return window;
+    }
+
 }
