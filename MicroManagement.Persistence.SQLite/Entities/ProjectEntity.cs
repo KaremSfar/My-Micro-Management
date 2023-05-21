715545a66fcdedccdc402dce17308fadb7a65769
Karem Sfar Gandoura <Karem.sfargandoura@expensya.com>
2023-05-21 15:44:09 +0100

    Add attributes to column entity, create first migartion with "Add Migration" and built DB with "Update Database"

--- MicroManagement.Persistence.SQLite/Entities/ProjectEntity.cs
+++ MicroManagement.Persistence.SQLite/Entities/ProjectEntity.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MicroManagement.Persistence.SQLite.Entities
 {
+    [Table("ProjectsTable")]
     public record ProjectEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Color { get; set; }
     }
 }
