5771bbc4a3a6a3172df581575986cb90843bfdc7
Karem Sfar Gandoura <Karem.sfargandoura@expensya.com>
2023-12-30 14:51:37 +0100

    add tailwind to project

--- Application.React/my-micro-management/src/App.tsx
+++ Application.React/my-micro-management/src/App.tsx
@@ -19,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Click me
+        </button>
       </header>
     </div>
   );
