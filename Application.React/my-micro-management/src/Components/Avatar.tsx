d81a503bed68ea12ceef3aa0fd8a4d6b71d1371a
Karem Sfar Gandoura <Karem.sfargandoura@expensya.com>
2024-02-24 13:38:39 +0100

    minor css tweaks

--- Application.React/my-micro-management/src/Components/Avatar.tsx
+++ Application.React/my-micro-management/src/Components/Avatar.tsx
@@ -11,8 +11,8 @@ function Avatar() {
         <div className="w-full max-w-sm">
             <Popover className="relative">
                 <Popover.Button className="focus:outline-none flex justify-between items-center text-gray-600 p-1 hover:bg-zinc-100 hover:text-gray-950 block rounded-md text-base font-medium">
-                    <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                        <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
+                    <div className="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                        <svg className="absolute w-10 h-10 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
                     </div>
                 </Popover.Button>
 
