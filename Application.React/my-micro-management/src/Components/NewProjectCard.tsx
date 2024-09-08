bdcf0443f00fd2f01e153fb7becf0ada83a5d159
Karem Sfar Gandoura <karemsfar@gmail.com>
2024-09-08 18:23:58 +0100

    minor on click swap

--- Application.React/my-micro-management/src/Components/NewProjectCard.tsx
+++ Application.React/my-micro-management/src/Components/NewProjectCard.tsx
@@ -17,6 +17,7 @@ function NewProjectCard({ onProjectCreated }: NewProjectCardProps) {
 
     return (
         <div
+            onClick={handleOpenModal}
             className="lg:aspect-[5/3] sm:min-w-48 border-2 rounded-lg shadow-md min-w-full m-1 hover:scale-[1.01] transition-transform hover:cursor-pointer bg-purple-400"
             style={{ borderColor: 'rgb(85, 0, 128)' }}>
             <div className="flex flex-col h-full justify-between font-bold p-2">
@@ -24,7 +25,7 @@ function NewProjectCard({ onProjectCreated }: NewProjectCardProps) {
                     Add new Activity
                 </span>
                 <div className="flex justify-center items-center flex-grow">
-                    <div className="border-2 border-white rounded-full p-2" onClick={handleOpenModal}>
+                    <div className="border-2 border-white rounded-full p-2">
                         <PlusIcon className="h-8 w-8 text-white" />
                     </div>
                 </div>
