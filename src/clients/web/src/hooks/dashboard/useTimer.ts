e9157648663b8a3dcec3fb5dd7df8e7c9a7fa5a0
KaremSfar <karemsfar@gmail.com>
2025-06-23 11:03:05 +0100

    update timers for browser snoozing

--- src/clients/web/src/hooks/dashboard/useTimer.ts
+++ src/clients/web/src/hooks/dashboard/useTimer.ts
@@ -1,23 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { ProjectSessionDTO } from "../../DTOs/ProjectDto";
 
-export const useTimer = (runningProjectId: string | null, setProjects: React.Dispatch<React.SetStateAction<ProjectSessionDTO[]>>) => {
+export const useTimer = (
+    runningProjectId: string | null,
+    setProjects: React.Dispatch<React.SetStateAction<ProjectSessionDTO[]>>
+) => {
+    let lastTimestamp = useRef<number>(Date.now());
+
     useEffect(() => {
-        if (!runningProjectId) return;
+        if (!runningProjectId)
+            return;
 
         const interval = setInterval(() => {
-            setProjects((projects) =>
-                projects.map((project) => {
-                    if (project.id !== runningProjectId) return project;
+            const now = Date.now();
+            const elapsedSeconds = Math.floor((now - lastTimestamp.current) / 1000);
 
-                    return {
-                        ...project,
-                        timeSpentCurrentSession: project.timeSpentCurrentSession + 1,
-                        timeSpentTotal: project.timeSpentTotal + 1,
-                    } as ProjectSessionDTO;
-                })
-            );
-        }, 1000);
+            if (elapsedSeconds > 0) {
+                lastTimestamp.current = now;
+
+                setProjects((projects) =>
+                    projects.map((project) => {
+                        if (project.id !== runningProjectId) return project;
+
+                        return {
+                            ...project,
+                            timeSpentCurrentSession: project.timeSpentCurrentSession + elapsedSeconds,
+                            timeSpentTotal: project.timeSpentTotal + elapsedSeconds,
+                        } as ProjectSessionDTO;
+                    })
+                );
+            }
+        }, 250);
+
+        // Reset the timer when starting
+        lastTimestamp.current = Date.now();
 
         return () => clearInterval(interval);
     }, [runningProjectId, setProjects]);
