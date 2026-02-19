d87fce8ed1822bc77bc230476a0a4eb5447c299a
KaremSfar <karemsfar@gmail.com>
2026-02-19 14:10:58 +0100

    update devops stuff

--- devops/update-files.sh
+++ devops/update-files.sh
@@ -17,7 +17,7 @@ sed -i "s#googleclient_id=.*#googleclient_id=$GOOGLE_CLIENT_ID#g" .env
 sed -i "s#googleclient_secret=.*#googleclient_secret=$GOOGLE_CLIENT_SECRET#g" .env
 sed -i "s#POSTGRES_USER=.*#POSTGRES_USER=$POSTGRES_USER#g" .env
 sed -i "s#POSTGRES_PASSWORD=.*#POSTGRES_PASSWORD=$POSTGRES_PASSWORD#g" .env
-sed -i "s#OTEL__ENDPOINT=.*#OTEL__ENDPOINT=$OTEL_JAEGER_URL#g" .env
+sed -i "s#OTEL__ENDPOINT=.*#OTEL__ENDPOINT=$OTEL__ENDPOINT#g" .env
 
 sed -i "s#REDIS__CONFIGURATION=.*#REDIS__CONFIGURATION=$REDIS__CONFIGURATION#g" .env
 sed -i "s#RABBITMQ_USER=.*#RABBITMQ_USER=$RABBITMQ_USER#g" .env
